f7b90ea60ede9f68a5c99a94ba5bb17abfde99df
ivankeller <ochun3161>
2019-11-10 22:43:40 +0100

    add test for add_frame

---
+++ tests/segmentboard/test_add_frame.py
@@ -0,0 +1,19 @@
+import unittest
+import numpy as np
+from tests.test_base_class import TestBaseClass
+from segmentboard.segmentboard import add_frame
+
+
+class TestAddFrame(TestBaseClass):
+    def test_add_frame(self):
+        img = np.arange(20).reshape((4, 5))
+        framed = add_frame(img, width=1, value=0)
+        expected = np.array([[0, 0, 0, 0, 0],
+                            [0, 6, 7, 8, 0],
+                            [0, 11, 12, 13, 0],
+                            [0, 0, 0, 0, 0]])
+        np.testing.assert_array_equal(framed, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
