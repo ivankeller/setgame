85dd52ed167f3885ebe7cea209270708f0f6611a
ivankeller <ochun3161>
2019-11-10 22:44:52 +0100

    renaming

--- tests/segmentboard/test_binarize_image.py
+++ tests/segmentboard/test_binarize_image.py
@@ -9,16 +9,16 @@ from segmentboard.segmentboard import binarize_image
 class TestBinarizeImage(TestBaseClass):
     def setUp(self):
         self.img = cv2.imread(os.path.join(self.RESOURCES_DIR, 'lenas_128.png'))
-        self.binarized = binarize_image(self.img)
+        self.binarized_img = binarize_image(self.img)
 
     def test_binary(self):
         """ Only 0 and 1 values in image"""
-        distinct_values = set(self.binarized.flatten())
+        distinct_values = set(self.binarized_img.flatten())
         self.assertEqual(distinct_values, set([0, 1]))
 
     def test_dimensions(self):
         """ If img.shape is (w, h, 3) for RGB format then binarized.shape should be (w, h)."""
-        self.assertEqual(self.img.shape[:2], self.binarized.shape)
+        self.assertEqual(self.img.shape[:2], self.binarized_img.shape)
 
 
 if __name__ == '__main__':
