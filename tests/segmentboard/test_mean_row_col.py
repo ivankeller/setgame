a5082b867347bbfccf8dd943c53920d0f902037a
ivankeller <ochun3161>
2019-11-11 13:13:42 +0100

    add test for mean_row_col

---
+++ tests/segmentboard/test_mean_row_col.py
@@ -0,0 +1,17 @@
+import unittest
+import numpy as np
+from tests.test_base_class import TestBaseClass
+from segmentboard.segmentboard import mean_row_col
+
+
+class TestMeanRowCol(TestBaseClass):
+    def test_dimensions(self):
+        img = np.random.randint(0, 256, size=(10, 6))
+        mean_row, mean_col = mean_row_col(img)
+        self.assertEqual(mean_row.ndim, 1)
+        self.assertEqual(mean_col.ndim, 1)
+        self.assertEqual(img.shape, (mean_col.size, mean_row.size))
+
+
+if __name__ == '__main__':
+    unittest.main()
