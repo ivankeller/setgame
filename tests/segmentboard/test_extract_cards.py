b27f7aef1e00c8b4c471e89d80bb80dbeee956dc
ivankeller <ochun3161>
2019-11-12 16:00:13 +0100

    add test extract cards with jpg and png

--- tests/segmentboard/test_extract_cards.py
+++ tests/segmentboard/test_extract_cards.py
@@ -7,16 +7,25 @@ from segmentboard.segmentboard import extract_cards
 
 
 class TestExtractCards(TestBaseClass):
+    def setUp(self):
+        self.jpg_img_path = os.path.join(self.FIXTURES_DIR, 'test_board.jpg')
+        self.png_img_path = os.path.join(self.FIXTURES_DIR, 'test_board.png')
 
-    def test_extract_cards_simple(self):
+    def _test_extract_cards(self, img_path):
         """Test on a test board image, nb of segmented cards and shapes."""
-        board_img = cv2.imread(os.path.join(self.FIXTURES_DIR, 'test_board.jpg'))
+        board_img = cv2.imread(img_path)
         cards = extract_cards(board_img)
         cards_shapes = [card.shape for card in cards]
         # number of cards
         self.assertEqual(len(cards), 12)
         # each card is a 3d array with third dimension equal to 3
         self.assertEqual(set([card_shape[2] for card_shape in cards_shapes]), {3})
+
+    def test_extract_cards_jpg(self):
+        self._test_extract_cards(self.jpg_img_path)
+
+    def test_extract_cards_png(self):
+        self._test_extract_cards(self.png_img_path)
 
 
 if __name__ == '__main__':
