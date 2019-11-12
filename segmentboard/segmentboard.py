4dd855c29e7fca7d8478b14110271f6abb2fca07
ivankeller <ochun3161>
2019-11-12 15:44:22 +0100

    clean docstrings and change print to loguru logging

--- segmentboard/segmentboard.py
+++ segmentboard/segmentboard.py
@@ -2,12 +2,13 @@
 Module to segment a board of cards into individual cards.
 Assumption: 
     - cards are aligned horizontally and vertically to the edges of the image
-    - the background is darker enough (TODO: test and try to quantify the minimum required contrast)
+    - the background is darker enough
 """
 
 import cv2
 import numpy as np
 from scipy.ndimage.filters import gaussian_filter1d
+from loguru import logger
 
 
 def binarize_image(img):
@@ -54,19 +55,20 @@ def add_frame(img, width=10, value=0):
 
 
 def mean_row_col(img, sigma=5):
-    """Return mean row and column of a 1-channel image, gaussian filtered.
+    """Average pixel values of rows and columns of a 1-channel image, gaussian filtered.
 
     Parameters
     ----------
-    img : 2d numpy array 
-        single channel image  
-    sigma : scalar (optional)
+    img : 2d numpy.ndarray
+        single channel image of shape (nb_of_rows, nb_of_columns)
+    sigma : scalar, optional
         standard deviation for Gaussian kernel
 
     Returns
     -------
     mean_row, mean_col : tuple of 1d numpy arrays
-        The mean row and mean column of the input image, filtered by Gaussian kernel for smoothing purposes.
+        The mean row and mean column of the input image, filtered by a Gaussian kernel for smoothing.
+        mean_row.size is nb_of_columns, and mean_col.size is nb_of_rows
 
     """
     mean_row = gaussian_filter1d(img.mean(axis=0), sigma)
@@ -75,25 +77,25 @@ def mean_row_col(img, sigma=5):
 
 
 def lower_cut_points(data, threshold):
-    """Return the indices of the nearest values below a given threshold (+/- 1 index value).
+    """Return the indices of a 1d array for the nearest values below a given threshold (+/- 1 index value).
 
     Ex: 
-        > arr = np.array([0., 0.1, 0.2, 0.5, 0.5, 0.3, 0.2, 0.1, 0.])
-        > segb.lower_cut_points(arr, threshold=0.25)
+        >>> arr = np.array([0., 0.1, 0.2, 0.5, 0.5, 0.3, 0.2, 0.1, 0.])
+        >>> lower_cut_points(arr, threshold=0.25)
         array([2, 5])
     The lack of accuracy of the index +/- 1 is not an issue for the present use case.
 
     Parameters
     ----------
-    data : 1d numpy array 
+    data : 1d numpy.ndarray
         input vector
     threshold : scalar
         lower cut threshold
 
     Returns
     -------
-    numpy array (int)
-        the indices corresponding to the first or last values below the threshold (+/- 1 index value)
+    numpy.ndarray (int)
+        the indices +/- 1 corresponding to the first or last values below the threshold.
 
     """
     binary = data < threshold
@@ -106,27 +108,29 @@ def crop(img, xmin, xmax, ymin, ymax):
 
     Parameters
     ----------
-    img : numpy array
+    img : 3d or 2d numpy.ndarray
         input image of shape (Y, X, n_channels) or (Y, X)
     xmin, xmax, ymin, ymax : int
         indices defining the rectangle to crop.
 
     Returns
     -------
-    cropped image : numpy array
+    cropped image : numpy.ndarray
         cropped image of shape (ymax - ymin, xmax - xmin, n_channels) or (ymax - ymin, xmax - xmin)
+        (xmin, ymin) corresponds to the upper-left crop point,
+        (xmax-1, ymax-1) corresponds to the lower-right crop point.
 
     """
     return img[ymin:ymax, xmin:xmax]
 
 
-def extract_cards(board_img, background_thres=0.25, verb=True):
-    """Return a list of individual cards from an image of the board.
+def extract_cards(board_img, background_thres=0.25):
+    """Return a list of individual segmented cards from an image of a board of cards.
 
     Parameters
     ----------
-    board_img : numpy array
-        image of the board
+    board_img : 3d numpy.ndarray
+        color image of the board
         Assumption: 
             - cards are aligned horizontally and vertically to the edges of the image
             - background is darker enough (TODO: test and try to quantify the min contrast)
@@ -135,11 +139,10 @@ def extract_cards(board_img, background_thres=0.25, verb=True):
 
     Returns
     -------
-    cards : list of numpy arrays
+    cards : list of numpy.ndarray
         the individual cards
 
     """
-
     framed_bin_img = add_frame(binarize_image(board_img))
     mean_row, mean_col = mean_row_col(framed_bin_img)
     xs_cuts = lower_cut_points(mean_row, background_thres).reshape((-1, 2))
@@ -149,6 +152,5 @@ def extract_cards(board_img, background_thres=0.25, verb=True):
         for ycut in ys_cuts:
             cropped = crop(board_img, *(xcut.tolist() + ycut.tolist()))
             cards.append(cropped)
-    if verb:
-        print("{0} cards segmented.".format(len(cards)))
+    logger.info(f"{len(cards)} cards segmented from board image.")
     return cards
