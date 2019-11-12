63ba837c429b739a6bc12a969f54ddf2c3b0f4bf
ivankeller <ochun3161>
2019-11-12 15:45:21 +0100

    better test_extract_cards with unittest

--- tests/segmentboard/test_extract_cards.py
+++ tests/segmentboard/test_extract_cards.py
@@ -1,13 +1,24 @@
-from segmentboard.segmentboard import extract_cards
-from utils.format import bgr2rgb
+import unittest
 import cv2
+import os
 
-TEST_IMG = "./test/test_data/test_board.jpg"
+from tests.test_base_class import TestBaseClass
+from segmentboard.segmentboard import extract_cards
 
 
-def test_extract_cards():
-    """Simple test. Todo: add more test"""
-    board_img = bgr2rgb(cv2.imread(TEST_IMG))
-    cards = extract_cards(board_img)
-    assert len(cards) == 12
+class TestExtractCards(TestBaseClass):
+
+    def test_extract_cards_simple(self):
+        """Test on a test board image, nb of segmented cards and shapes."""
+        board_img = cv2.imread(os.path.join(self.FIXTURES_DIR, 'test_board.jpg'))
+        cards = extract_cards(board_img)
+        cards_shapes = [card.shape for card in cards]
+        # number of cards
+        self.assertEqual(len(cards), 12)
+        # each card is a 3d array with third dimension equal to 3
+        self.assertEqual(set([card_shape[2] for card_shape in cards_shapes]), {3})
+
+
+if __name__ == '__main__':
+    unittest.main()
 
