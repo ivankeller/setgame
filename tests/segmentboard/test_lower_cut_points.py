7a781cd9a2cea1442253933b8d08dff51fb3a29e
ivankeller <ochun3161>
2019-11-12 15:44:39 +0100

    typo

--- tests/segmentboard/test_lower_cut_points.py
+++ tests/segmentboard/test_lower_cut_points.py
@@ -5,7 +5,7 @@ from segmentboard.segmentboard import lower_cut_points
 
 
 class TestLowerCutPoints(TestBaseClass):
-    def test_loer_cut_points(self):
+    def test_lower_cut_points(self):
         arr = np.array([0., 0.1, 0.2, 0.5, 0.5, 0.3, 0.2, 0.1, 0.])
         result = lower_cut_points(arr, threshold=0.25)
         expected = np.array([2, 5])
