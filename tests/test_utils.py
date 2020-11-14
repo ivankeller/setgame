84b20be51d723ba9f3368387bdc245c1aa7e7e7d
ivankeller <ochun3161>
2020-11-15 00:12:08 +0100

    remove all scratch notebooks from repo

---
+++ tests/test_utils.py
@@ -0,0 +1,29 @@
+import unittest
+from tests.test_base_class import TestBaseClass
+import utils
+
+
+class TestUtils(TestBaseClass):
+    def test_list_files_with_extension(self):
+        directory = f'{self.RESOURCE_DIR}/utils'
+        extension = '.PNG'
+        result = utils.list_files_with_extension(directory, extension)
+        expected = [f'{directory}/{file}' for file in ['image2.PNG', 'image3.PNG']]
+        self.assertListEqual(result, expected)
+
+    def test_list_files_with_extension_return_empty(self):
+        directory = f'{self.RESOURCE_DIR}/utils'
+        extension = '.py'
+        result = utils.list_files_with_extension(directory, extension)
+        expected = []
+        self.assertListEqual(result, expected)
+
+    def test_list_images_in_directory(self):
+        directory = f'{self.RESOURCE_DIR}/utils'
+        result = utils.list_images_in_directory(directory)
+        expected = [f'{directory}/{file}' for file in ['image1.jpg', 'image2.PNG', 'image3.PNG']]
+        self.assertListEqual(result, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
--- utils.py
+++ utils.py
@@ -1,4 +1,6 @@
 import cv2
+from glob import glob
+from typing import List
 
 
 def bgr2rgb(img):
@@ -33,5 +35,32 @@ def read_image(path):
 
     """
     return bgr2rgb(cv2.imread(path))
+
+
+def list_files_with_extension(directory: str, extension: str) -> List[str]:
+    """List all files in directory with given extension.
+
+    Parameters
+    ----------
+    directory : str
+        path to a directory without final '/'
+    extension : str
+        extension including the '.'
+
+    Returns
+    -------
+    List of path of the files in the directory with given extension.
+
+    """
+    return glob(f'{directory}/*{extension}')
+
+
+def list_images_in_directory(directory: str, extensions: List[str] = ['jpg', 'JPG', 'JPEG', 'png', 'PNG']) -> List[str]:
+    """List all image files in directory, according to list of extensions."""
+    image_files = []
+    for extension in extensions:
+        image_files += list_files_with_extension(directory=directory, extension=extension)
+    return image_files
+
 
 
