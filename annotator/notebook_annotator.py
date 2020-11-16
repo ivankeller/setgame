25e1bbd5567954ee7ec1c3aef7dd3bbd5861072d
ivankeller <ochun3161>
2020-11-16 23:10:33 +0100

    default values for Label refactor annotator notebook, simpler

--- annotator/notebook_annotator.py
+++ annotator/notebook_annotator.py
@@ -6,7 +6,7 @@
 #       extension: .py
 #       format_name: light
 #       format_version: '1.5'
-#       jupytext_version: 1.7.0
+#       jupytext_version: 1.5.2
 #   kernelspec:
 #     display_name: setgame
 #     language: python
@@ -14,37 +14,31 @@
 # ---
 
 # # Annotate Set game cards
+# Interractive annotator tool for Set card image  
 
-# ## config
+# ## configuration
 
 # +
-# examples to annotate: list of path or path to directory with card images
+# examples to annotate: list of path or directory with card images
 examples = [
     '../../datatest/examples_for_annotator/IMG_20170518_204728_0.png', 
     '../../datatest/examples_for_annotator/IMG_20170518_205001_7.png',
 ]
-
 #examples = '../../datatest/examples_for_annotator'
+
+# output directory for annotation saved as json files
+# If None annotations are saved in default `labels` sub-directory 
+output_dir = None
 # -
 
 # ## run the annotator
 
-import sys
-sys.path.append('..')
-
 # %load_ext autoreload
 # %autoreload 2
 
+import sys
+sys.path.append('..')
 from annotator import Annotator
 
 annotator = Annotator(examples)
-
-annotator.examples
-
 annotator.annotate()
-
-annotator.annotations
-
-
-
-
